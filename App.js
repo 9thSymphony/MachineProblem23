ddc7c40260cbb062659aef61efc3b72fac8ac9f0
Ryan Karlyle <bordadora67@gmail.com>
2023-05-18 09:03:22 +0800

    Machine Problem 3 & 2

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {Button} from "@react-native-material/core";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Colina biot</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => (
+  <Button title = "Press me" onPress={() => alert("UwU")}/>
+);
+export default App;
