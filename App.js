33b75eb7e469f2de5e76a69bb93a8886deb7b1df
9th Symphony <bordadora67@gmail.com>
2023-05-18 20:37:57 +0800

    Second Try

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
-import React from "react";
-import {Button} from "@react-native-material/core";
+import { StyleSheet, View } from "react-native";
+import { Button } from "@react-native-material/core";
 
 const App = () => (
-  <Button title = "Press me" onPress={() => alert("UwU")}/>
+  <View style={styles.container}>
+    <Button title="Login" onPress={() => alert("Please enter your username and password")} />
+    <Button title="Register" onPress={() => alert("Please enter credentials")} />  
+  </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
 export default App;
